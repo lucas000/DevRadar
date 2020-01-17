fecdb4d3f44a62507afc8eabdc82c70ad0b23ee8
Lucas Lourenço de Jesus <lucasdejesus000@gmail.com>
2020-01-16 21:44:04 -0300

    Upload the frontend
    File index with form to sign in with styles created

---
+++ frontend/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import './global.css';
+import './App.css';
+import './SideBar.css';
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required></input>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              
+            </header>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
